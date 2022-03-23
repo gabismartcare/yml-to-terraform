09ef39be44d43ad8e087f5f1501aabc49790d7e6
Thomas Hayen <thayen@gmail.com>
2022-03-23 16:14:11 +0100

    fix test

--- main_test.go
+++ main_test.go
@@ -9,6 +9,6 @@ func TestCompiler(t *testing.T) {
 	os.Setenv("POSTGRES_PASSWORD", "pg_value")
 	os.Setenv("BUILD_VERSION", "version")
 	os.Setenv("PROJECT_ID", "projet")
-	transformToTerraform("/home/tom/IdeaProjects/gabi/integration/pediarity/pediarity.yml", "./test/output")
+	transformToTerraform("./test/input/test-file.yml", "./test/output")
 
 }
